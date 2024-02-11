219a77fa966baee33614afd0e4f91bad94a4703e
anilhazar <alianilhazar@gmail.com>
2024-02-11 14:51:29 +0300

    Uploaded Leetcode LongestSubstringWithoutRepeatingCharachters Solution

---
+++ Java/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,29 @@
+import java.util.HashSet;
+
+public class LongestSubstringWithoutRepeatingCharacters {
+    public static void main(String[] args) {
+
+        String s = "abdfca";
+        HashSet<Character> hashSet = new HashSet();
+        int length = s.length();
+        int max = 0;
+        char tempChar;
+
+        for (int i = 0; i < length; i++) {
+
+            for (int j = 0; j < length; j++) {
+
+                tempChar = s.charAt(j);
+
+                if (!(hashSet.contains(tempChar))) {
+                    hashSet.add(tempChar);
+                    max = Math.max(max, hashSet.size());
+                } else {
+                    break;
+                }
+            }
+            hashSet.clear();
+        }
+        System.out.println("Length of Longest Substring Without Repeating Characters is: " + max);
+    }
+}
